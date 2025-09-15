047224325a8fe77498a2225c38c598f0ec8f9f7a
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2025-09-15 13:29:54 +0000

    Initial commit

---
+++ src/pages/Inicio.c1dmp.js
@@ -0,0 +1,10 @@
+// Referencia de API: https://www.wix.com/velo/reference/api-overview/introduction
+// “Hello, World!” Ejemplo: https://learn-code.wix.com/en/article/1-hello-world
+
+$w.onReady(function () {
+	// Write your JavaScript here
+
+	// To select an element by ID use: $w('#elementID')
+
+	// Click 'Preview' to run your code
+});
